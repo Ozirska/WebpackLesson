029d5d893d449c52dcba74cab6dd97bf18ac9dea
Ozirska <m.ozirska2000@gmail.com>
2023-03-06 14:27:48 +0100

    ESlint plugin

--- lesson9/task1/.eslintrc.js
+++ lesson9/task1/.eslintrc.js
@@ -2,12 +2,7 @@ module.exports = {
   extends: "eslint-config-airbnb-base",
   env: {
     browser: true,
-    es6: true,
   },
-  extends: "eslint:recommended",
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
+
   rules: {},
 };
